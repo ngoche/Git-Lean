e116adaa7bf810367d9207cde9b0af5fa01e6748
Hệ Ngọc Văn <ngoche@users.noreply.github.com>
2020-10-03 15:08:10 +0700

    transition element

--- rew3design/script.js
+++ rew3design/script.js
@@ -1,10 +1,22 @@
-var Henv = {
+let Henv = {
 	openLeftSide:function(){
-		console.log('check run');
-		var sideLeft = document.getElementsByClassName('side-left')[0];
-		if(sideLeft.style.display === 'none')
-			sideLeft.style.display = 'block';
-		else
-			sideLeft.style.display = 'none';
+		let sideLeft = document.getElementsByClassName('side-left')[0];
+		let sizeSideLeft = sideLeft.style.width.replace('%','');
+		sizeSideLeft = parseInt(sizeSideLeft);
+		
+		let container = document.getElementsByClassName('container')[0];
+		let footer = document.getElementById('footer');
+		if(sizeSideLeft < '20' ){
+			sideLeft.style.width = '20%';
+			container.style.width = '80%';
+			footer.style.width = '80%';
+		}
+		else{
+			sideLeft.style.width = '0%';
+			container.style.width = '100%';
+			footer.style.width = '100%';
+		}
 	},
+	changeLeftMenuPaddingTop: function(){
+	}
 }
