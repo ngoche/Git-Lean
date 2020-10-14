56e15442b4c80901cd50c366d495ec68f46713ed
Hệ Ngọc Văn <ngoche@users.noreply.github.com>
2020-10-14 23:09:29 +0700

    change files

--- rew3design/script.js
+++ rew3design/script.js
@@ -5,16 +5,13 @@ let Henv = {
 		sizeSideLeft = parseInt(sizeSideLeft);
 		
 		let container = document.getElementsByClassName('container')[0];
-		let footer = document.getElementById('footer');
 		if(sizeSideLeft < '20' ){
 			sideLeft.style.width = '20%';
 			container.style.width = '80%';
-			footer.style.width = '80%';
 		}
 		else{
 			sideLeft.style.width = '0%';
 			container.style.width = '100%';
-			footer.style.width = '100%';
 		}
 	},
 	changeLeftMenuPaddingTop: function(){
