f8cefae2d1d9d0f2a00328a3ef2745384dbb572d
Hệ Ngọc Văn <ngoche@users.noreply.github.com>
2021-09-23 13:07:09 +0700

    cm

--- JavStu/src/jav/study/thread/ThreadSync.java
+++ JavStu/src/jav/study/thread/ThreadSync.java
@@ -6,12 +6,31 @@ public class ThreadSync extends Thread {
 	public ThreadSync() {
 		this.tongHang = 9999;
 	}
-	public void muaHang(int soLuong) {
+	public synchronized void muaHang(int soLuong) {
 		if(tongHang > 0) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			tongHang = tongHang - soLuong ;
 			System.out.println("So hang con lai : "+tongHang);
 		}else {
 			System.out.println("hethang");
 		}
+	}
+	@Override
+	public void run() {
+		muaHang(9999);
+	}
+	public static void main(String[] args) {
+		ThreadSync t = new ThreadSync();
+		
+		Thread t1 = new Thread(t);
+		Thread t2 = new Thread(t);
+		
+		t1.start();
+		t2.start();
 	}
 }
